224719b0a2c848c88e3078503c0dea2a8a92f1a8
Wizock <silent.death3500@gmail.com>
2022-03-05 05:05:56 +1000

    individual and non-commital ifs

--- image/qrapi.py
+++ image/qrapi.py
@@ -14,6 +14,9 @@ base_url = "https://api.discordapp.fun/"
 @server.route("/qr")
 def qr_api():
     args = request.args
+    qr_text = args.get("text")
+    qr_url = args.get("qrcode")
+    print(qr_text, qr_url)
     if not args.get("text") and not args.get("qrcode"):
         return {
             "StatusCode": 400,
@@ -22,23 +25,11 @@ def qr_api():
                 "Text": base_url + "qr?&text=https://www.youtube.com/watch?v=dQw4w9WgXcQ",
                 "Qrcode": base_url + "qr?&qrcode=image_url"
             }
-        }
-    if args.get("text"):
-        qr_text = args["text"]
-        qr = qrcode.QRCode(
-            version=1,
-            box_size=10,
-            border=1)
-        qr.add_data(qr_text)
-        qr.make(fit=True)
-        img = qr.make_image(fill='black', back_color='white')
-        img_io = BytesIO()
-        img.save(img_io, 'JPEG', quality=70)
-        img_io.seek(0)
-        return send_file(img_io, mimetype='image/jpeg')
+        }   
 
-    if args.get("qrcode"):
-        qr_url = args["qrcode"]
+    if qr_url == None:
+        return 'Invalid qr url'
+    else:
         image_formats = ("image/png", "image/jpeg", "image/gif")
         try:
             site = requests.get(qr_url)
@@ -56,7 +47,6 @@ def qr_api():
                 "StatusCode": 400,
                 "Message": "Please request with valid image url"
             }
-
         req = Request(qr_url, headers={'User-Agent': "Magic Browser"})
         resource = urlopen(req)
         img_bytes = BytesIO(resource.read())
@@ -73,3 +63,18 @@ def qr_api():
             "StatusCode": 200,
             "Message": val
         }
+
+    if qr_text == None or 0 or len(qr_text) == 0:
+        return 'Invalid qr text'
+    else:
+        qr = qrcode.QRCode(
+            version=1,
+            box_size=10,
+            border=1)
+        qr.add_data(qr_text)
+        qr.make(fit=True)
+        img = qr.make_image(fill='black', back_color='white')
+        img_io = BytesIO()
+        img.save(img_io, 'JPEG', quality=70)
+        img_io.seek(0)
+        return send_file(img_io, mimetype='image/jpeg')
