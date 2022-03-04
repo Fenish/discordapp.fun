a8a3f35daf139049e7603f3681ee2812f2a43491
Talha Şöhret <fenishwastaken@gmail.com>
2022-03-05 00:11:30 +0300

    update qr api syntax

--- image/qrapi.py
+++ image/qrapi.py
@@ -4,8 +4,9 @@ import requests
 import numpy as np
 
 from io import BytesIO
-from flask import request, send_file, Blueprint
 from urllib.request import Request, urlopen
+from flask import request, send_file, Blueprint
+
 
 server = Blueprint("qr_api", __name__)
 base_url = "https://api.discordapp.fun/"
@@ -13,26 +14,32 @@ base_url = "https://api.discordapp.fun/"
 
 @server.route("/qr")
 def qr_api():
-    args = request.args
-    qr_text = args.get("text")
-    qr_url = args.get("qrcode")
-    print(qr_text, qr_url)
-    if not args.get("text") and not args.get("qrcode"):
+    params = request.args
+    param_create = params.get("create")
+    param_read = params.get("read")
+
+    if not param_create and not param_read:
         return {
             "StatusCode": 400,
             "Message": "Invalid parameters",
             "Parameters": {
-                "Text": base_url + "qr?&text=https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-                "Qrcode": base_url + "qr?&qrcode=image_url"
+                "Create QR Code": base_url + "qr?create=https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                "Read QR code": base_url + "qr?read=qrcode_url"
             }
-        }   
+        }
+    if param_read:
+        qr_code = read_qr(param_read)
+        return qr_code
+    elif param_create:
+        qr_code = create_qr(param_create)
+        return send_file(qr_code, mimetype='image/jpeg')
 
-    if qr_url == None:
-        return 'Invalid qr url'
-    else:
+
+def read_qr(image_url):
+    if image_url:
         image_formats = ("image/png", "image/jpeg", "image/gif")
         try:
-            site = requests.get(qr_url)
+            site = requests.get(image_url)
         except (requests.exceptions.MissingSchema,
                 requests.exceptions.ConnectionError,
                 requests.exceptions.InvalidSchema):
@@ -47,7 +54,8 @@ def qr_api():
                 "StatusCode": 400,
                 "Message": "Please request with valid image url"
             }
-        req = Request(qr_url, headers={'User-Agent': "Magic Browser"})
+
+        req = Request(image_url, headers={'User-Agent': "Magic Browser"})
         resource = urlopen(req)
         img_bytes = BytesIO(resource.read())
         file_bytes = np.asarray(bytearray(img_bytes.read()), dtype=np.uint8)
@@ -64,17 +72,16 @@ def qr_api():
             "Message": val
         }
 
-    if qr_text == None or 0 or len(qr_text) == 0:
-        return 'Invalid qr text'
-    else:
-        qr = qrcode.QRCode(
-            version=1,
-            box_size=10,
-            border=1)
-        qr.add_data(qr_text)
-        qr.make(fit=True)
-        img = qr.make_image(fill='black', back_color='white')
-        img_io = BytesIO()
-        img.save(img_io, 'JPEG', quality=70)
-        img_io.seek(0)
-        return send_file(img_io, mimetype='image/jpeg')
+
+def create_qr(text):
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=1)
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    img_io = BytesIO()
+    img.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return img_io
